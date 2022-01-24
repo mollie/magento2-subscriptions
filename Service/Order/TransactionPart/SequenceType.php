8cd2c26e14480c086cea94ec755a8e28afa6a1b0
Michiel Gerritsen <michiel@controlaltdelete.nl>
2022-01-24 11:24:07 +0100

    Feature: Allow to place recurring orders when Magento Vault is used

--- Service/Order/TransactionPart/SequenceType.php
+++ Service/Order/TransactionPart/SequenceType.php
@@ -7,6 +7,7 @@
 namespace Mollie\Subscriptions\Service\Order\TransactionPart;
 
 use Magento\Sales\Api\Data\OrderInterface;
+use Mollie\Payment\Config;
 use Mollie\Payment\Model\Client\Orders;
 use Mollie\Payment\Model\Client\Payments;
 use Mollie\Payment\Service\Order\TransactionPartInterface;
@@ -15,13 +16,20 @@ use Mollie\Subscriptions\Service\Order\OrderContainsSubscriptionProduct;
 class SequenceType implements TransactionPartInterface
 {
     /**
+     * @var Config
+     */
+    private $config;
+
+    /**
      * @var OrderContainsSubscriptionProduct
      */
     private $orderContainsSubscriptionProduct;
 
     public function __construct(
+        Config $config,
         OrderContainsSubscriptionProduct $orderContainsSubscriptionProduct
     ) {
+        $this->config = $config;
         $this->orderContainsSubscriptionProduct = $orderContainsSubscriptionProduct;
     }
 
@@ -31,12 +39,53 @@ class SequenceType implements TransactionPartInterface
             return $transaction;
         }
 
+        if ($this->shouldBeRecurringSequence($order)) {
+            return $this->setSequenceTypeRecurring($apiMethod, $transaction);
+        }
+
+        return $this->setSequenceTypeFirst($apiMethod, $transaction);
+    }
+
+    private function shouldBeRecurringSequence(OrderInterface $order): bool
+    {
+        if (
+            !$this->config->isMagentoVaultEnabled($order->getStoreId()) ||
+            !$order->getPayment() ||
+            !$order->getPayment()->getExtensionAttributes() ||
+            !$order->getPayment()->getExtensionAttributes()->getVaultPaymentToken()
+        ) {
+            return false;
+        }
+
+        $paymentToken = $order->getPayment()->getExtensionAttributes()->getVaultPaymentToken();
+        if ($order->getPayment()->getMethod() != $paymentToken->getPaymentMethodCode()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function setSequenceTypeFirst(string $apiMethod, array $transaction): array
+    {
         if ($apiMethod == Payments::CHECKOUT_TYPE) {
             $transaction['sequenceType'] = 'first';
         }
 
         if ($apiMethod == Orders::CHECKOUT_TYPE) {
             $transaction['payment']['sequenceType'] = 'first';
+        }
+
+        return $transaction;
+    }
+
+    protected function setSequenceTypeRecurring(string $apiMethod, array $transaction): array
+    {
+        if ($apiMethod == Payments::CHECKOUT_TYPE) {
+            $transaction['sequenceType'] = 'recurring';
+        }
+
+        if ($apiMethod == Orders::CHECKOUT_TYPE) {
+            $transaction['payment']['sequenceType'] = 'recurring';
         }
 
         return $transaction;

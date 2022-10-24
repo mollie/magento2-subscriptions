0c25a504a7d1af3b87373a15f24f2efa6e67261a
Michiel Gerritsen <michiel@controlaltdelete.nl>
2022-10-24 13:59:30 +0200

    Feature: Prevent duplicate subscriptions

---
+++ Service/Cart/CustomerAlreadyHasSubscriptionToProduct.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mollie\Subscriptions\Service\Cart;
+
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Exception\NotFoundException;
+use Mollie\Subscriptions\Api\SubscriptionToProductRepositoryInterface;
+
+class CustomerAlreadyHasSubscriptionToProduct
+{
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
+     * @var SubscriptionToProductRepositoryInterface
+     */
+    private $subscriptionToProductRepository;
+
+    public function __construct(
+        CustomerRepositoryInterface $customerRepository,
+        SubscriptionToProductRepositoryInterface $subscriptionToProductRepository
+    ) {
+        $this->subscriptionToProductRepository = $subscriptionToProductRepository;
+        $this->customerRepository = $customerRepository;
+    }
+
+    public function execute(Customer $customer, ProductInterface $product): bool
+    {
+        try {
+            $customerData = $this->customerRepository->getById($customer->getId());
+            $mollieCustomerId = $customerData->getExtensionAttributes()->getMollieCustomerId();
+
+            if (!$mollieCustomerId) {
+                return false;
+            }
+
+            $this->subscriptionToProductRepository->getByCustomerIdAndProductId($mollieCustomerId, $product->getId());
+            return true;
+        } catch (NotFoundException $e) {
+            return false;
+        }
+    }
+}
